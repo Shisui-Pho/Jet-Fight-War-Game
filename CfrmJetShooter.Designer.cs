a194c0d1bae991bb41df0f7ac2e9384979faa092
Shisui-Pho <110053217+Shisui-Pho@users.noreply.github.com>
2022-08-29 12:05:53 +0200

    Add files via upload

---
+++ CfrmJetShooter.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Jet_Shooter_Game_by_Phiwo
+{
+    partial class CfrmJetShooter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picJet = new System.Windows.Forms.PictureBox();
+            this.picEnemy1 = new System.Windows.Forms.PictureBox();
+            this.picEnemy2 = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.picBullet = new System.Windows.Forms.PictureBox();
+            this.tmrStarGame = new System.Windows.Forms.Timer(this.components);
+            this.picEnemy3 = new System.Windows.Forms.PictureBox();
+            this.lblCoordinates = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picJet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBullet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picJet
+            // 
+            this.picJet.BackgroundImage = global::Jet_Shooter_Game_by_Phiwo.Properties.Resources.player;
+            this.picJet.Location = new System.Drawing.Point(267, 484);
+            this.picJet.Name = "picJet";
+            this.picJet.Size = new System.Drawing.Size(110, 101);
+            this.picJet.TabIndex = 0;
+            this.picJet.TabStop = false;
+            // 
+            // picEnemy1
+            // 
+            this.picEnemy1.Image = global::Jet_Shooter_Game_by_Phiwo.Properties.Resources.enemy;
+            this.picEnemy1.Location = new System.Drawing.Point(52, -3);
+            this.picEnemy1.Name = "picEnemy1";
+            this.picEnemy1.Size = new System.Drawing.Size(100, 85);
+            this.picEnemy1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picEnemy1.TabIndex = 1;
+            this.picEnemy1.TabStop = false;
+            // 
+            // picEnemy2
+            // 
+            this.picEnemy2.Image = global::Jet_Shooter_Game_by_Phiwo.Properties.Resources.enemy;
+            this.picEnemy2.Location = new System.Drawing.Point(512, -3);
+            this.picEnemy2.Name = "picEnemy2";
+            this.picEnemy2.Size = new System.Drawing.Size(100, 85);
+            this.picEnemy2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picEnemy2.TabIndex = 2;
+            this.picEnemy2.TabStop = false;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(302, 230);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(25, 25);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "0";
+            // 
+            // picBullet
+            // 
+            this.picBullet.Image = global::Jet_Shooter_Game_by_Phiwo.Properties.Resources.bullet;
+            this.picBullet.Location = new System.Drawing.Point(320, 484);
+            this.picBullet.Name = "picBullet";
+            this.picBullet.Size = new System.Drawing.Size(7, 27);
+            this.picBullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picBullet.TabIndex = 4;
+            this.picBullet.TabStop = false;
+            // 
+            // tmrStarGame
+            // 
+            this.tmrStarGame.Interval = 40;
+            this.tmrStarGame.Tick += new System.EventHandler(this.tmrStarGame_Tick);
+            // 
+            // picEnemy3
+            // 
+            this.picEnemy3.Image = global::Jet_Shooter_Game_by_Phiwo.Properties.Resources.enemy;
+            this.picEnemy3.Location = new System.Drawing.Point(277, -3);
+            this.picEnemy3.Name = "picEnemy3";
+            this.picEnemy3.Size = new System.Drawing.Size(100, 85);
+            this.picEnemy3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picEnemy3.TabIndex = 5;
+            this.picEnemy3.TabStop = false;
+            // 
+            // lblCoordinates
+            // 
+            this.lblCoordinates.AutoSize = true;
+            this.lblCoordinates.Location = new System.Drawing.Point(615, 561);
+            this.lblCoordinates.Name = "lblCoordinates";
+            this.lblCoordinates.Size = new System.Drawing.Size(66, 13);
+            this.lblCoordinates.TabIndex = 6;
+            this.lblCoordinates.Text = "Co-ordinates";
+            // 
+            // CfrmJetShooter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(684, 573);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblCoordinates);
+            this.Controls.Add(this.picJet);
+            this.Controls.Add(this.picBullet);
+            this.Controls.Add(this.picEnemy2);
+            this.Controls.Add(this.picEnemy3);
+            this.Controls.Add(this.picEnemy1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CfrmJetShooter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jet Shooter";
+            this.Load += new System.EventHandler(this.CfrmJetShooter_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            this.MouseCaptureChanged += new System.EventHandler(this.CoordinatesForCode);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.GetCoordinates);
+            ((System.ComponentModel.ISupportInitialize)(this.picJet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBullet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picEnemy3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picJet;
+        private System.Windows.Forms.PictureBox picEnemy1;
+        private System.Windows.Forms.PictureBox picEnemy2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.PictureBox picBullet;
+        private System.Windows.Forms.Timer tmrStarGame;
+        private System.Windows.Forms.PictureBox picEnemy3;
+        private System.Windows.Forms.Label lblCoordinates;
+    }
+}
+
---
+++ CfrmJetShooter.cs
@@ -0,0 +1,240 @@
+﻿/*Developer name     : Phiwokwakhe Khathwane 
+ * Available at      : phiwokwakhe299@gmail.com
+ * Git-Hub Id        : @Shisui-Pho
+ * Stack OverFlow Id : @Shisui-Pho
+ * Zapsplat          : @The_Collector
+ * Project Type      : Game 
+ * Project name      : Fighter Jet (Mod Version )
+*/
+
+using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Jet_Shooter_Game_by_Phiwo
+{
+    
+   
+    public partial class CfrmJetShooter : Form
+    {
+        bool goLeft, goRight, isShouting, isGameOver;
+
+        int iPlayerSpeed = 12, iBulletSpeed , iEnemySpeed, iScore;
+        Random rndEnemyPosition = new Random();
+        //int iNumber1 = 2;
+	  int iNumber1 = 2;
+	  int iCountScore = 0;
+
+        private void CoordinatesForCode(object sender, EventArgs e)
+        {
+
+        }//CoordinatesForCode
+
+        private void GetCoordinates(object sender, MouseEventArgs e)
+        {
+            string sCoordinates = "( " + e.X.ToString() + " ; " + e.Y.ToString() + " )";
+            lblCoordinates.Text = sCoordinates;
+        }//GetCoordinates
+
+        public CfrmJetShooter()
+        {
+            InitializeComponent();
+            gameOver(iNumber1);
+            Playing();
+           // resetGame();
+        }//Initializing the components
+
+        private void CfrmJetShooter_Load(object sender, EventArgs e)
+        {
+
+        }//CfrmaLoad
+
+        private void tmrStarGame_Tick(object sender, EventArgs e)
+        {
+            /*SoundPlayer splyPlaySong = new SoundPlayer(Properties.Resources.Play2);
+            splyPlaySong.Play();*/
+            //PlaySound();
+            
+            picEnemy1.Visible = true;
+            picEnemy2.Visible = true;
+            picEnemy3.Visible = true;
+            lblScore.Text = iScore.ToString();
+
+            picEnemy1.Top += iEnemySpeed;
+            picEnemy2.Top += iEnemySpeed;
+            picEnemy3.Top += iEnemySpeed;
+
+            if (picEnemy1.Top > 556 || picEnemy2.Top > 556 || picEnemy3.Top > 556)
+            {
+                gameOver();
+            }//If the enemy jet hits the ground , then the game ends
+
+            if (goLeft && picJet.Left > 0)
+            {
+                picJet.Left -= iPlayerSpeed;
+            }//Check if the left key is pressed
+            if(goRight && picJet.Left < 570)
+            {
+                picJet.Left += iPlayerSpeed;
+            }//Check if the player pressed the right key
+            if (isShouting)
+            {
+                iBulletSpeed = 20;
+                picBullet.Top -= iBulletSpeed;
+            }//
+            else
+            {
+                picBullet.Left = -300;
+                iBulletSpeed = 0;
+               // picBullet.Top = picJet.Top;
+            }//If the space key is not pressed remove the bullet from the game
+
+            if(picBullet.Top < -30)
+            {
+                isShouting = false;
+            }//
+
+            if (picBullet.Bounds.IntersectsWith(picEnemy1.Bounds))
+            {
+                //Hit();
+                iScore += 1;
+                picEnemy1.Left = rndEnemyPosition.Next(0, 600);
+                picEnemy1.Top = -470;
+                isShouting = false;
+		        iCountScore+= 1;
+
+            }///If the bullet intersect with the enemy jets number 1 , then the score will be incremented and the enemy jet will dissapear and appear on a random place
+            if (picBullet.Bounds.IntersectsWith(picEnemy2.Bounds))
+            {
+                //Hit();
+                iScore += 1;
+                picEnemy2.Left = rndEnemyPosition.Next(0, 600);
+                picEnemy2.Top = -470;
+                isShouting = false;
+		        iCountScore+= 1;
+
+            }///If the bullet intersect with the enemy jets number 1 , then the score will be incremented and the enemy jet will dissapear and appear on a random place
+            if (picBullet.Bounds.IntersectsWith(picEnemy3.Bounds))
+            {
+                //Hit();
+                iScore += 1;
+                picEnemy3.Left = rndEnemyPosition.Next(0, 600);
+                picEnemy3.Top = -470;
+                isShouting = false;
+		        iCountScore+= 1;
+
+            }///If the bullet intersect with the enemy jets number 1 , then the score will be incremented and the enemy jet will dissapear and appear on a random place
+            //Increase the score if the player hits a certain score
+
+	     
+	        if(iCountScore == 20)
+	        {
+                //Increase speed by 2
+	 	        iEnemySpeed += 2 ;
+                iPlayerSpeed += 2;
+		
+		        iCountScore = 0;
+	        }//Loop to increase the speed every 20 points score	     
+
+        }//tmrStarGame_Tick
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }//If the left key is pressed then assigne the bool variable of Left to true and the control can move to the left 
+            if(e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }//If the left key is pressed then assigne the bool variable of Right to true and the control can move to the Right 
+
+        }//Key is pressed
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }//If the player releases the button stop the execution of the button clicked
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }//If the player releases the button stop the execution of the button clicked
+
+            if(e.KeyCode == Keys.Space)
+            {
+                isShouting = true;
+                picBullet.Top = picJet.Top;
+                picBullet.Left = picJet.Left + picJet.Width / 2;
+            }//If the space key is pressed and released the start shooting 
+            if(e.KeyCode == Keys.Enter)
+            {
+                resetGame();
+            }//If the enter key is pressed and released then the game will restart
+        }//KeyIsUp
+        private void resetGame()
+        {
+            //Start the timer
+            tmrStarGame.Start();
+
+            //Initialize the positions of enemy jets based on the top position random 
+            picEnemy1.Top = rndEnemyPosition.Next(-20, 80);
+            picEnemy2.Top = rndEnemyPosition.Next(-20, 80);
+            picEnemy3.Top = rndEnemyPosition.Next(-20, 80);
+
+            //Initialize the position of enemy jets based on the horizontal position
+            picEnemy1.Left = rndEnemyPosition.Next(0, 600);
+            picEnemy2.Left = rndEnemyPosition.Next(0, 600);
+            picEnemy3.Left = rndEnemyPosition.Next(0, 600);
+
+            //Initialize the bullet to an initial position
+            picBullet.Left = -300;
+
+            //Inititialize all every variable
+            iCountScore = 0;
+            iScore = 0;
+            lblScore.Text = iScore.ToString();
+            iBulletSpeed = 0;
+            iEnemySpeed = 6;
+            isShouting = false;
+
+        }//Reset the game
+        private void gameOver(int _iNumber1 = 1)//int _iNumber1 = 1)
+        {
+            
+            if(_iNumber1 == 1)
+            {
+                isGameOver = true;
+                tmrStarGame.Stop();
+
+                lblScore.Text =  Environment.NewLine + "Score : " + iScore + "\nGame Over !!!"  + Environment.NewLine + "Press enter to restart the game ";
+            }
+            else
+            {
+                //Start();
+                picEnemy1.Visible = false;
+                picEnemy2.Visible = false;
+                picEnemy3.Visible = false;
+                tmrStarGame.Stop();
+                lblScore.Text = "Welcome to Phiwo's".PadLeft(100)+"Jet Shooter game" + Environment.NewLine + "\nPress enter to start the game ";
+            }           
+        }//GameOver
+        private void Hit()
+        {
+            SoundPlayer splyPlaySong = new SoundPlayer(Properties.Resources.hit);//new SoundPlayer(Lococation : "@c:\Users\Phiwo\Desktop\hit.wav")//|| ("@c:\Users\Phiwo\Desktop\hit.wav");
+            splyPlaySong.Play();
+        }//Play hit sound
+        private void Start()
+        {
+            SoundPlayer splyPlaySong = new SoundPlayer(Properties.Resources.play);
+            splyPlaySong.Play();
+        }//Play brgining sound
+        private void Playing()
+        {
+            SoundPlayer splyPlaySong = new SoundPlayer(Properties.Resources.Japan_Life1);
+            splyPlaySong.PlayLooping();
+           //splyPlaySong.Loo
+        }//Play playing sound
+    }//End of Class
+}//End of namespace
